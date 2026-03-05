0ffe89e02a8d77dbc0d3d4ba1568590c23d50376
alexandrr2222 <237439209+alexandrr2222@users.noreply.github.com>
2026-03-05 14:46:14 +0200

    Refactor winCon and fix gameBoardPieces privacy

--- script.js
+++ script.js
@@ -8,71 +8,71 @@ const gameBoard = (() => {
         }
         gameBoardPieces[tempPrompt] = player.mark;
         displayBoard();
-        gameController.endCondition()
     }
     const displayBoard = () => {
-        console.log(gameBoardPieces.slice(0, 3).toString(), gameBoardPieces.slice(3, 6).toString(), gameBoardPieces.slice(6, 9).toString(), "#################")
+        console.log("#################\n", gameBoardPieces.slice(0, 3).toString(), "\n", gameBoardPieces.slice(3, 6).toString(), "\n", gameBoardPieces.slice(6, 9).toString(), "\n #################")
     }
-    return {gameBoardPieces, claimPiece, displayBoard};
+    const resetBoard = () => {
+        gameBoardPieces.fill(" ");
+    }
+    const winCondition = (mark) => {
+        const [p1, p2, p3, p4, p5, p6, p7, p8, p9] = gameBoardPieces;
+        if(
+        //Rows
+        ([p1, p2, p3].every(piece => piece === mark)) ||
+        ([p4, p5, p6].every(piece => piece === mark)) ||
+        ([p7, p8, p9].every(piece => piece === mark)) ||
+        //Columns
+        ([p1, p4, p7].every(piece => piece === mark)) ||
+        ([p2, p5, p8].every(piece => piece === mark)) ||
+        ([p3, p6, p9].every(piece => piece === mark)) ||
+        //Diagonal
+        ([p1, p5, p9].every(piece => piece === mark)) ||
+         ([p3, p5, p7].every(piece => piece === mark))
+        ){
+            return true;
+        }
+        return false;
+    }
+    const fullBoardCheck = () => {
+        if(gameBoardPieces.every(p => p !== " ")){
+            return true;
+        }
+        return false;
+    }
+    return {claimPiece, resetBoard, winCondition, fullBoardCheck};
 })()
 
 const gameController = (() => {
-  let breaker = false;
   const player1 = makePlayer("player1", "X");
   const player2 = makePlayer("player2", "O");
   const initiate = () => {
     for(let i = 0; i < 9; i++){
-      if(breaker)break;
-      gameBoard.claimPiece(player1);
-      if(breaker)break;
-      gameBoard.claimPiece(player2);
+        gameBoard.claimPiece(player1);
+        if(gameBoard.winCondition(player1.mark)){
+            console.log("playa1 won")
+            gameBoard.resetBoard();
+            break;
+        }
+        else if(gameBoard.fullBoardCheck()){
+            console.log("tie");
+            gameBoard.resetBoard();
+            break;
+        }
+        gameBoard.claimPiece(player2);
+        if(gameBoard.winCondition(player2.mark)){
+            console.log("playa2 won")
+            gameBoard.resetBoard();
+            break;
+        }
+        else if(gameBoard.fullBoardCheck()){
+            console.log("tie");
+            gameBoard.resetBoard();
+            break;
+        }
     }
   }
-  const endCondition = () => {
-    const [p1, p2, p3, p4, p5, p6, p7, p8, p9] = gameBoard.gameBoardPieces;
-      if(
-        //Rows
-        ([p1, p2, p3].every(piece => piece === "X")) ||
-        ([p4, p5, p6].every(piece => piece === "X")) ||
-        ([p7, p8, p9].every(piece => piece === "X")) ||
-        //Columns
-        ([p1, p4, p7].every(piece => piece === "X")) ||
-        ([p2, p5, p8].every(piece => piece === "X")) ||
-        ([p3, p6, p9].every(piece => piece === "X")) ||
-        //Diagonal
-        ([p1, p5, p9].every(piece => piece === "X")) ||
-         ([p3, p5, p7].every(piece => piece === "X"))
-        ){
-        console.log("X win");
-        player1.score += 1;
-        breaker = true;
-        gameBoard.gameBoardPieces.fill(" ");
-        }
-      else if(
-        //Rows
-        ([p1, p2, p3].every(piece => piece === "O")) ||
-        ([p4, p5, p6].every(piece => piece === "O")) ||
-        ([p7, p8, p9].every(piece => piece === "O")) ||
-        //Columns
-        ([p1, p4, p7].every(piece => piece === "O")) ||
-        ([p2, p5, p8].every(piece => piece === "O")) ||
-        ([p3, p6, p9].every(piece => piece === "O")) ||
-        //Diagonal
-        ([p1, p5, p9].every(piece => piece === "O")) ||
-         ([p3, p5, p7].every(piece => piece === "O"))
-        ){
-        console.log("O win");
-        breaker = true;
-        player2.score += 1;
-        gameBoard.gameBoardPieces.fill(" ");
-        }
-      else if(gameBoard.gameBoardPieces.every(p => p !== " ")){
-            console.log("tie");
-            breaker = true
-            gameBoard.gameBoardPieces.fill(" ");
-      }
-    }
-  return {initiate, endCondition}
+  return {initiate}
 })()
 
 function makePlayer(name, mark){
