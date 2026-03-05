42b99eb088eac697f54d5a452dba65b876ca9722
Oleksandr <237439209+alexandrr2222@users.noreply.github.com>
2026-03-05 02:07:23 +0200

    Finish console version

--- script.js
+++ script.js
@@ -1,96 +1,85 @@
 const gameBoard = (() => {
-    let gameBoardPieces = ["", "", "", "", "", "", "", "", ""];
+    let gameBoardPieces = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     const claimPiece = (player) => {
         let tempPrompt = Number(prompt("write number from 0 to 8"));
-        if(gameBoardPieces[tempPrompt] !== ""){
+        while(gameBoardPieces[tempPrompt] !== " "){
             console.log("error, that piece is claimed")
             tempPrompt = Number(prompt("write number from 0 to 8"));
         }
-        else gameBoardPieces[tempPrompt] = player.mark;
+        gameBoardPieces[tempPrompt] = player.mark;
+        displayBoard();
+        gameController.endCondition()
     }
     const displayBoard = () => {
-        console.log(gameBoardPieces.slice(0, 3))
-        console.log(gameBoardPieces.slice(3, 6))
-        console.log(gameBoardPieces.slice(6, 9))
-        console.log("#################")
+        console.log(gameBoardPieces.slice(0, 3).toString(), gameBoardPieces.slice(3, 6).toString(), gameBoardPieces.slice(6, 9).toString(), "#################")
     }
     return {gameBoardPieces, claimPiece, displayBoard};
 })()
 
 const gameController = (() => {
-    const toggle = () => {
-        let turnToggle = true;
-        if(turnToggle){
-            turnToggle = false;
-        }
-        else{
-            turnToggle = true;
-        }
+  let breaker = false;
+  const player1 = makePlayer("player1", "X");
+  const player2 = makePlayer("player2", "O");
+  const initiate = () => {
+    for(i = 0; i < 9; i++){
+      if(breaker)break;
+      gameBoard.claimPiece(player1);
+      if(breaker)break;
+      gameBoard.claimPiece(player2);
     }
-    const endCondition = () => {
-        if(
-            (gameBoard.gameBoardPieces[0] === "X") && (gameBoard.gameBoardPieces[1] === "X") && (gameBoard.gameBoardPieces[2] === "X")
+  }
+  const endCondition = () => {
+    const [p1, p2, p3, p4, p5, p6, p7, p8, p9] = gameBoard.gameBoardPieces;
+      if(
+        //Rows
+        ([p1, p2, p3].every(piece => piece === "X")) ||
+        ([p4, p5, p6].every(piece => piece === "X")) ||
+        ([p7, p8, p9].every(piece => piece === "X")) ||
+        //Columns
+        ([p1, p4, p7].every(piece => piece === "X")) ||
+        ([p2, p5, p8].every(piece => piece === "X")) ||
+        ([p3, p6, p9].every(piece => piece === "X")) ||
+        //Diagonal
+        ([p1, p5, p9].every(piece => piece === "X")) ||
+         ([p3, p5, p7].every(piece => piece === "X"))
         ){
-            console.log("win");
-            gameBoard.gameBoardPieces.fill("");
+        console.log("X win");
+        player1.score += 1;
+        breaker = true;
+        gameBoard.gameBoardPieces.fill(" ");
         }
-        else if(gameBoard.gameBoardPieces.every(p => p !== "")){
+      else if(
+        //Rows
+        ([p1, p2, p3].every(piece => piece === "O")) ||
+        ([p4, p5, p6].every(piece => piece === "O")) ||
+        ([p7, p8, p9].every(piece => piece === "O")) ||
+        //Columns
+        ([p1, p4, p7].every(piece => piece === "O")) ||
+        ([p2, p5, p8].every(piece => piece === "O")) ||
+        ([p3, p6, p9].every(piece => piece === "O")) ||
+        //Diagonal
+        ([p1, p5, p9].every(piece => piece === "O")) ||
+         ([p3, p5, p7].every(piece => piece === "O"))
+        ){
+        console.log("O win");
+        breaker = true;
+        player2.score += 1;
+        gameBoard.gameBoardPieces.fill(" ");
+        }
+      else if(gameBoard.gameBoardPieces.every(p => p !== " ")){
             console.log("tie");
-            gameBoard.gameBoardPieces.fill("");
-        }
-        else console.log("nothing yet")
+            breaker = true
+            gameBoard.gameBoardPieces.fill(" ");
+      }
     }
-    return {toggle, endCondition}
+  return {initiate, endCondition}
 })()
 
 function makePlayer(name, mark){
     return{
         name,
-        mark
+        mark,
+        score: 0
     }
 }
-const john = makePlayer("player1", "X");
-const peter = makePlayer("player2", "O")
-
-gameBoard.claimPiece(john);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(peter);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(john);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(peter);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(john);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(peter);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(john);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(peter);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(john);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(peter);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(john);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(peter);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(john);
-gameBoard.displayBoard()
-gameController.endCondition()
-gameBoard.claimPiece(peter);
-gameBoard.displayBoard()
-gameController.endCondition()
+gameController.initiate();
