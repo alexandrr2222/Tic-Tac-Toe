50576d5b93c39ffdb8d86b6b00a6f324d6c035b3
alexandrr2222 <237439209+alexandrr2222@users.noreply.github.com>
2026-03-14 01:03:22 +0200

    Start rewriting logic for DOM version

--- game.js
+++ game.js
@@ -1,16 +1,22 @@
 const gameBoard = (() => {
     let gameBoardPieces = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-    const claimPiece = (player) => {
-        let tempPrompt = Number(prompt("write number from 0 to 8"));
-        while(gameBoardPieces[tempPrompt] !== " "){
-            console.log("error, that piece is claimed")
-            tempPrompt = Number(prompt("write number from 0 to 8"));
-        }
-        gameBoardPieces[tempPrompt] = player.mark;
+    const claimPiece = (player, id) => {
+        
+        const drawPiece = document.querySelector(`#${id}`);
+        if(player.mark = "X")drawPiece.textContent = "X";
+        else if(player.mark = "O")drawPiece.textContent = "O";
+        gameBoardPieces[id] = player.mark;
+
+        // let tempPrompt = Number(prompt("write number from 0 to 8"));
+        // while(gameBoardPieces[tempPrompt] !== " "){
+        //     console.log("error, that piece is claimed")
+        //     tempPrompt = Number(prompt("write number from 0 to 8"));
+        // }
+        // gameBoardPieces[tempPrompt] = player.mark;
         displayBoard();
     }
     const displayBoard = () => {
-        console.log("#################\n", gameBoardPieces.slice(0, 3).toString(), "\n", gameBoardPieces.slice(3, 6).toString(), "\n", gameBoardPieces.slice(6, 9).toString(), "\n #################")
+        // console.log("#################\n", gameBoardPieces.slice(0, 3).toString(), "\n", gameBoardPieces.slice(3, 6).toString(), "\n", gameBoardPieces.slice(6, 9).toString(), "\n #################")
     }
     const resetBoard = () => {
         gameBoardPieces.fill(" ");
@@ -44,44 +50,118 @@ const gameBoard = (() => {
 })()
 
 const gameController = (() => {
-  const player1 = makePlayer("player1", "X");
-  const player2 = makePlayer("player2", "O");
-  const initiate = () => {
+    let player1 = null;
+    let player2 = null;
+    
+    const setUp = () => {
+        const player1Name = localStorage.getItem("player1Name");
+        const player2Name = localStorage.getItem("player2Name");
+        const player1Type = localStorage.getItem("player1Type");
+        const player2Type = localStorage.getItem("player2Type");
+        const player1Mark = localStorage.getItem("player1Mark");
+        const player2Mark = localStorage.getItem("player2Mark");
+        if(player1Mark === "X"){
+            player1 = makePlayer(player1Name, player1Type, player1Mark);
+            player2 = makePlayer(player2Name, player2Type, player2Mark);
+        }
+        else{
+            player1 = makePlayer(player2Name, player2Type, player2Mark);
+            player2 = makePlayer(player1Name, player1Type, player1Mark);
+        }
+        const insertPlayer1Name = document.querySelector(".playerName1");
+        const insertPlayer2Name = document.querySelector(".playerName2");
+        insertPlayer1Name.textContent = player1.name;
+        insertPlayer2Name.textContent = player2.name;
+    }
+
+    const setListeners = () => {
+        let toggle = false;
+        pieces.forEach(pc => {
+                pc.addEventListener("click", () => {
+                    id = Number(pc.id);
+                    toggle = !toggle
+                    if(toggle){
+                        gameBoard.claimPiece(player1, id)
+                    }
+                    else if(!toggle){
+                        gameBoard.claimPiece(player2, id)
+                    }
+                });
+        });
+    }
+
+    const initiate = () => {
+    const currentPlayer = document.querySelector(".currentTurnPlayerNameInsert")
+    const winNumber1 = document.querySelector(".winNumber1");
+    const winNumber2 = document.querySelector(".winNumber2");
+    const winText1 = document.querySelector(".winText1");
+    const winText2 = document.querySelector(".winText2");
+    const currentTurnX = document.querySelector(".currentTurnX");
+    const currentTurnO = document.querySelector(".currentTurnO");
+    const xShadow = document.querySelector(".xShadow");
+    const oShadow = document.querySelector(".oShadow");
+    currentPlayer.style.color = "#555E62"
+    const pieces = document.querySelectorAll(".piece");
     for(let i = 0; i < 9; i++){
+        currentPlayer.textContent = `${player1.name}'S TURN`;
+        currentTurnX.classList.add("opacityUpX");
+        currentTurnO.classList.remove("opacityUpO");
+        xShadow.classList.remove("hider");
+        oShadow.classList.add("hider");
+
+
         gameBoard.claimPiece(player1);
         if(gameBoard.winCondition(player1.mark)){
-            console.log("player1 won");
+            // console.log(player1.name, " won");
+            currentPlayer.style.color = "#00EEC5"
+            currentPlayer.textContent = `${player1.name} CLAIMS VICTORY!`;
             player1.score += 1;
-            console.log(`Player 1 current score is: ${player1.score} \n Player 2 current score is: ${player2.score}`);
+            if(player1.score === 1) winText1.textContent = "WIN"
+            else winText1.textContent = "WINS"
+            winNumber1.textContent = player1.score;
+            // console.log(`Player 1 current score is: ${player1.score} \n Player 2 current score is: ${player2.score}`);
             gameBoard.resetBoard();
             break;
         }
         else if(gameBoard.fullBoardCheck()){
-            console.log("tie");
+            currentPlayer.textContent = "DRAW";
+            // console.log("tie");
             gameBoard.resetBoard();
             break;
         }
+        currentPlayer.textContent = `${player2.name}'S TURN`;
+        currentTurnX.classList.remove("opacityUpX");
+        currentTurnO.classList.add("opacityUpO");
+        xShadow.classList.add("hider");
+        oShadow.classList.remove("hider");
         gameBoard.claimPiece(player2);
         if(gameBoard.winCondition(player2.mark)){
-            console.log("player2 won");
+            // console.log("player2 won");
+            currentPlayer.style.color = "#E6D172"
+            currentPlayer.textContent = `${player1.name} CLAIMS VICTORY!`;
             player2.score += 1;
-            console.log(`Player 1 current score is: ${player1.score} \n Player 2 current score is: ${player2.score}`);
+            if(player1.score === 1) winText2.textContent = "WIN"
+            else winText2.textContent = "WINS"
+            winNumber2.textContent = player1.score;
+            // console.log(`Player 1 current score is: ${player1.score} \n Player 2 current score is: ${player2.score}`);
             gameBoard.resetBoard();
             break;
         }
-        else if(gameBoard.fullBoardCheck()){
-            console.log("tie");
-            gameBoard.resetBoard();
-            break;
-        }
+        // else if(gameBoard.fullBoardCheck()){
+        //     currentPlayer.textContent = "DRAW";
+        //     console.log("tie");
+        //     gameBoard.resetBoard();
+        //     break;
+        // }
     }
   }
-  return {initiate}
+  return {setUp, setListeners, initiate}
 })()
 
-function makePlayer(name, mark){
+function makePlayer(name, type, mark){
     return{
         name,
+        type,
         mark,
         score: 0
     }
@@ -90,3 +170,8 @@ function makePlayer(name, mark){
 const DOMLogic = {
 
 }
+
+function nig(){
+
+}
+gameController.setUp()
--- menu.js
+++ menu.js
@@ -1,3 +1,12 @@
+// custom select menu
+const selectMenu = (() => {
+
+})()
+// switch to select menu
+
+
+
+// X-O switch
 const player1_X = document.querySelector("#x1");
 const player1_O = document.querySelector("#o1");
 const player2_X = document.querySelector("#x2");
@@ -5,21 +14,49 @@ const player2_O = document.querySelector("#o2");
 const XOs = document.querySelectorAll(".xoSelector");
 XOs.forEach(element => {
     element.addEventListener("click", () => {
-        if(element.id === "o1" && player2_O.checked === true){
+        if(element.id === "o1"){
             player2_O.checked = false;
             player2_X.checked = true;
         }
-        else if(element.id === "o2" && player1_O.checked === true){
+        else if(element.id === "o2"){
             player1_O.checked = false;
             player1_X.checked = true;
         }
-        else if(element.id === "x1" && player2_X.checked === true){
+        else if(element.id === "x1"){
             player2_O.checked = true;
             player2_X.checked = false;
         }
-        else if(element.id === "x2" && player1_X.checked === true){
+        else if(element.id === "x2"){
             player1_O.checked = true;
             player1_X.checked = false;
         }
     });
+});
+// Start Button and data saving
+const name1 = document.querySelector("#name1");
+const name2 = document.querySelector("#name2");
+const start = document.querySelector(".startButton");
+start.addEventListener("click", () => {
+    if(name1.value.trim() === "" || name2.value.trim() === ""){
+        const warning = document.querySelector(".warning");
+        if(warning){
+            warning.classList.remove("warning");
+            warning.offsetWidth;
+            warning.classList.add("warning");
+            return;
+        }
+        else{
+            start.insertAdjacentHTML("beforebegin", `<div class="warning">NAME REQUIRED</div>`);
+            return;
+        }
+    }
+    localStorage.setItem("player1Name", document.querySelector("#name1").value.toUpperCase());
+    localStorage.setItem("player2Name", document.querySelector("#name2").value.toUpperCase());
+
+    localStorage.setItem("player1Type", document.querySelector(".HumRobSel1:checked").value);
+    localStorage.setItem("player2Type", document.querySelector(".HumRobSel2:checked").value);
+
+    localStorage.setItem("player1Mark", document.querySelector(".xoSel1:checked").value);
+    localStorage.setItem("player2Mark", document.querySelector(".xoSel2:checked").value);
+    window.location.href = "game.html";
 });
